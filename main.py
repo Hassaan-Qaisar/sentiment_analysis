48f7a3b6d9d7ef8b54c5bc79377151322023bc76
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2024-01-23 22:01:31 +0500

    Update model and tokenizer to roberta base.

--- main.py
+++ main.py
@@ -1,63 +1,40 @@
 from transformers import AutoModelForSequenceClassification
 from transformers import TFAutoModelForSequenceClassification
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoConfig
 import numpy as np
 from scipy.special import softmax
-import csv
-import urllib.request
-
 # Preprocess text (username and link placeholders)
 def preprocess(text):
     new_text = []
- 
- 
     for t in text.split(" "):
         t = '@user' if t.startswith('@') and len(t) > 1 else t
         t = 'http' if t.startswith('http') else t
         new_text.append(t)
     return " ".join(new_text)
-
-# Tasks:
-# emoji, emotion, hate, irony, offensive, sentiment
-# stance/abortion, stance/atheism, stance/climate, stance/feminist, stance/hillary
-
-task='sentiment'
-MODEL = f"cardiffnlp/twitter-roberta-base-{task}"
-
+MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
-
-# download label mapping
-labels=[]
-mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task}/mapping.txt"
-with urllib.request.urlopen(mapping_link) as f:
-    html = f.read().decode('utf-8').split("\n")
-    csvreader = csv.reader(html, delimiter='\t')
-labels = [row[1] for row in csvreader if len(row) > 1]
-
+config = AutoConfig.from_pretrained(MODEL)
 # PT
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
-model.save_pretrained(MODEL)
-
-text = "Good night 😊"
+#model.save_pretrained(MODEL)
+text = "Covid cases are increasing fast!"
 text = preprocess(text)
 encoded_input = tokenizer(text, return_tensors='pt')
 output = model(**encoded_input)
 scores = output[0][0].detach().numpy()
 scores = softmax(scores)
-
 # # TF
 # model = TFAutoModelForSequenceClassification.from_pretrained(MODEL)
 # model.save_pretrained(MODEL)
-
-# text = "Good night 😊"
+# text = "Covid cases are increasing fast!"
 # encoded_input = tokenizer(text, return_tensors='tf')
 # output = model(encoded_input)
 # scores = output[0][0].numpy()
 # scores = softmax(scores)
-
+# Print labels and scores
 ranking = np.argsort(scores)
 ranking = ranking[::-1]
 for i in range(scores.shape[0]):
-    l = labels[ranking[i]]
+    l = config.id2label[ranking[i]]
     s = scores[ranking[i]]
     print(f"{i+1}) {l} {np.round(float(s), 4)}")
