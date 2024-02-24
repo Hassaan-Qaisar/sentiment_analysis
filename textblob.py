33aa3cb5a4cd4d26e2468526ea6aa8c2386e879f
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2024-02-24 22:31:38 +0500

    Count sentiment of tweets in the database with textblob.

--- textblob.py
+++ textblob.py
@@ -1,8 +1,27 @@
 # checking another way to do sentiment analysis
 from textblob import TextBlob
+from dbconnection import data
 
-text = "This is a great tutorial on sentiment analysis!"
-blob = TextBlob(text)
+positive_count = 0
+negative_count = 0
+neutral_count = 0
 
-sentiment = blob.sentiment
-print(sentiment)
+for sentence in data:
+    blob = TextBlob(sentence)
+    sentiment = blob.sentiment
+
+    # Determine sentiment polarity
+    polarity = sentiment.polarity
+
+    # Classify tweets based on polarity
+    if polarity > 0.2:
+        positive_count += 1
+    elif polarity < 0:
+        negative_count += 1
+    else:
+        neutral_count += 1
+
+# Print counts
+print(f"Positive tweets: {positive_count}")
+print(f"Negative tweets: {negative_count}")
+print(f"Neutral tweets: {neutral_count}")
